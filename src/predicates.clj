876c510219f2dcde90eb8d4e16304e3a7b18ffa7
Vapaavuori <jussi-pekka.vapaavuori@cgi.com>
2017-12-08 15:00:43 +0200

    end parts

--- src/predicates.clj
+++ src/predicates.clj
@@ -25,17 +25,18 @@
   :-)
 
 (defn has-award? [book award]
-  :-)
+  (boolean (award (:awards book))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (:awards book) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+   (first (filter boolean (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter boolean (map (complement pred) a-seq))))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= (mod n x) 0))]
+    (not (some pred (range 2 n)))))
 ;^^
